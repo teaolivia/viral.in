60eeace5bc69761cd67dc5fd1da932295259524b
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-05-31 03:09:48 +0700

    change jsx root into Route

--- viralin/src/App.js
+++ viralin/src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <p>Hello, world!</p>
-      </div>
+      <Route>
+        <div className="App">
+          <p>Hello, world!</p>
+        </div>
+      </Route>      
     );
   }
 }
