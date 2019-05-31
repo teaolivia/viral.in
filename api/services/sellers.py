9ce38163c5c6153961e0d2beef7ddb4c9e69eeb4
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-06-01 01:01:01 +0700

    getter get

--- api/services/sellers.py
+++ api/services/sellers.py
@@ -5,40 +5,40 @@ import requests
 
 app = Flask(__name__)
 
-business_owners = [
-    {
-        'id': 1,
-        'username': 'asdf',
-        'password': '1234'
-        'business_name': u'ASD',
-        'description': u'Kami adalah usaha dengan menjual barang elektronik', 
-        'email': 'asd@abc.com',
-        'phone': '+6281234567890',
-        'contents': {
-            'content_type': [
+with open("{}/database/sellers.json".format(root_dir()), "r") as f:
+    sellers = json.load(f)
 
-            ],
-            'content_title': '',
-            'duration': 20,
-            'form': [
-                'fisik',
-                'digital'
-            ],
-            'asset': 999999999,
-            'games_rule': 'naldfghahgargawjg;arjg;lasjg'
+@app.route("/", methods=['POST'])
+def body():
+    return nice_json({
+        "uri": "/",
+        "subresource_uris": {
+            "sellers": "/sellers",
+            "username": "/sellers/<userid>/<username>",
+            "promotors": "/sellers/<userid>/promotors",
+            "contents": "/sellers/<userid>/contents"
         }
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+    })
 
-@app.route('/business/api/v1.0/business', methods=['POST'])
-def get_all():
-    return jsonify({'business_owners': business_owners})
+# basic getter route
+@app.route('/sellers', methods=['POST'])
+def get_sellers():
+    return nice_json(sellers)
 
+@app.route('/sellers/<userid>/<username>', methods=['POST'])
+def get_sellers_username(username):
+    if username not in sellers:
+        return "404 Not Found"
+    return nice_json(sellers[userid])
+
+@app.route('/sellers/<userid>/promotors', methods=['POST'])
+def get_sellers_promotors():
+    return nice_json(sellers[promotors])
+
+@app.route('/sellers/<userid>/contents', methods=['POST'])
+def get_sellers_contents():
+    return nice_json(sellers[contents])
+
+# main driver
 if __name__ == '__main__':
     app.run(debug=True)
