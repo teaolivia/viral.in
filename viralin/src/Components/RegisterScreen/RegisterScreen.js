57dd47635d4a9cc8c3b7030e3c524b6282ec02ce
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 06:14:02 +0700

    Refactor RegisterScreen Component to fullfill eslint

--- viralin/src/Components/RegisterScreen/RegisterScreen.js
+++ viralin/src/Components/RegisterScreen/RegisterScreen.js
@@ -1,79 +1,77 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+
 import React from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
 
 import 'Components/RegisterScreen/RegisterScreen.css';
 
 import TabContainer from 'Components/TabContainer/TabContainer';
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabValue: 0,
+    };
+    this.changeTabValue = this.changeTabValue.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            tabValue : 0,
-            username : '',
-        };
-        this.changeTabValue = this.changeTabValue.bind(this);
-    }
-    
-    changeTabValue(event, newValue) {
-        this.setState({tabValue : newValue});
-    }
+  changeTabValue(event, newValue) {
+    this.setState({ tabValue: newValue });
+  }
 
-    render () {
-        return (
-            <div className="LoginScreen">
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                    spacing={0}
-                >    
-                    <Grid item xs={12}>
-                        <Typography className="Header" variant="h1" component="h2">
-                            Daftar
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}><br /></Grid>
-                    <Grid item xs={3}></Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Container> 
-                            <Paper className = "Paper">
-                                <Tabs value={this.state.tabValue}
-                                    onChange={this.changeTabValue}
-                                >
-                                    <Tab label="Akun Bisnis" />
-                                    <Tab label="Akun Promotor" />
-                                </Tabs>
-                                <TabContainer>
-                                    <Button variant="contained" color="primary" className="Button">
-                                        <Typography variant="subtitle1">Register</Typography>
-                                    </Button>
-                                </TabContainer>                                                                
-                            </Paper>                                       
-                            <Typography variant="subtitle1">
-                                <br />
-                                Promosi? Viralin ajaaa..
-                            </Typography>
-                        </Container>
-                    </Grid>
-                    <Grid item xs={3}></Grid>
-                    
-                </Grid>
-            </div>
-        );        
-    }
+  render() {
+    const { tabValue } = this.state;
+    return (
+      <div className="LoginScreen">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item xs={12}>
+            <Typography className="Header" variant="h1" component="h2">
+              Daftar
+            </Typography>
+          </Grid>
+          <Grid item xs={12}><br /></Grid>
+          <Grid item xs={3} />
+          <Grid item xs={12} sm={6}>
+            <Container>
+              <Paper className="Paper">
+                <Tabs
+                  value={tabValue}
+                  onChange={this.changeTabValue}
+                >
+                  <Tab label="Akun Bisnis" />
+                  <Tab label="Akun Promotor" />
+                </Tabs>
+                <TabContainer>
+                  <Button variant="contained" color="primary" className="Button">
+                    <Typography variant="subtitle1">Register</Typography>
+                  </Button>
+                </TabContainer>
+              </Paper>
+              <Typography variant="subtitle1">
+                <br />
+                Promosi? Viralin ajaaa..
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid item xs={3} />
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default LoginScreen;
-;
