0f7ac68e24a6a851c264022e10239d15f6e32d90
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 05:46:54 +0700

    refactoring for fullfilled eslint

--- viralin/src/Components/LoginScreen/LoginScreen.js
+++ viralin/src/Components/LoginScreen/LoginScreen.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -8,108 +9,120 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
+// eslint-disable-next-line import/no-unresolved
 import 'Components/LoginScreen/LoginScreen.css';
 
+// eslint-disable-next-line import/no-unresolved
 import TabContainer from 'Components/TabContainer/TabContainer';
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabValue: 0,
+      username: '',
+    };
+    this.changeTabValue = this.changeTabValue.bind(this);
+    this.changeUsernameValue = this.changeUsernameValue.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            tabValue : 0,
-            username : '',
-        };
-        this.changeTabValue = this.changeTabValue.bind(this);
-        this.changeUsernameValue = this.changeUsernameValue.bind(this);
-    }
-    
-    changeTabValue(event, newValue) {
-        this.setState({tabValue : newValue});
-    }
+  changeTabValue(event, newValue) {
+    this.setState({ tabValue: newValue });
+  }
 
-    changeUsernameValue(event) {        
-        this.setState({username : event.target.value});
-    }
+  changeUsernameValue(event) {
+    this.setState({ username: event.target.value });
+  }
 
-    render () {
-        return (
-            <div className="LoginScreen">
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                    spacing={0}
-                >    
-                    <Grid item xs={12}>
-                        <Typography className="Header" variant="h1" component="h2">
-                            VIRALIN
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}><br /></Grid>
-                    <Grid item xs={3}></Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Container> 
-                            <Paper className = "Paper">
-                                <Tabs value={this.state.tabValue}
-                                    onChange={this.changeTabValue}
-                                >
-                                    <Tab label="Akun Bisnis" />
-                                    <Tab label="Akun Promotor" />
-                                    <Tab label="Akun Admin" />
-                                </Tabs>
-                                <TabContainer>
-                                    {(this.state.tabValue === 0 || this.state.tabValue === 1) &&  <TextField
-                                            id="filled-username"
-                                            label="Username"
-                                            className="TextField"
-                                            margin="normal"
-                                            variant="filled"
-                                            fullWidth
-                                            onChange={this.changeUsernameValue}
-                                    />}
-                                    <TextField
-                                            id="filled-password"
-                                            label="Password"
-                                            className="TextField"
-                                            margin="normal"
-                                            variant="filled"
-                                            type="password"
-                                            fullWidth
-                                            onChange={this.changeUsernameValue}
-                                    />
-                                    <Button variant="contained" color="primary" className="Button">
-                                        <Typography variant="subtitle1">Login</Typography>
-                                    </Button>
-                                    {(this.state.tabValue === 0 || this.state.tabValue === 1) &&                                      
-                                    <Box component="span">
-                                        <br />
-                                        <br />
-                                        <Link to="/forgot-password" className="Button">Forgot Password?</Link>
-                                        &nbsp; | &nbsp; 
-                                        <Link to="/register" className="Button">Register</Link>
-                                    </Box>
-                                    }
-                                </TabContainer>                                                                
-                            </Paper>           
-                            {(this.state.tabValue === 0 || this.state.tabValue === 1) && 
-                                <Typography variant="subtitle1">
-                                    <br />
-                                    Promosi? Viralin ajaaa..
-                                </Typography>
-                            } 
-                        </Container>
-                    </Grid>
-                    <Grid item xs={3}></Grid>
-                    
-                </Grid>
-            </div>
-        );        
-    }
+  render() {
+    const { tabValue, username } = this.state;
+    return (
+      <div className="LoginScreen">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item xs={12}>
+            <Typography className="Header" variant="h1" component="h2">
+                VIRALIN
+            </Typography>
+          </Grid>
+          <Grid item xs={12}><br /></Grid>
+          <Grid item xs={3} />
+          <Grid item xs={12} sm={6}>
+            <Container>
+              <Paper className="Paper">
+                <Tabs
+                  value={tabValue}
+                  onChange={this.changeTabValue}
+                >
+                  <Tab label="Akun Bisnis" />
+                  <Tab label="Akun Promotor" />
+                  <Tab label="Akun Admin" />
+                </Tabs>
+                <TabContainer>
+                  {
+                    (tabValue === 0 || tabValue === 1)
+                      && (
+                      <TextField
+                        id="filled-username"
+                        label="Username"
+                        className="TextField"
+                        margin="normal"
+                        variant="filled"
+                        fullWidth
+                        value={username}
+                        onChange={this.changeUsernameValue}
+                      />
+                      )}
+                  <TextField
+                    id="filled-password"
+                    label="Password"
+                    className="TextField"
+                    margin="normal"
+                    variant="filled"
+                    type="password"
+                    fullWidth
+                    onChange={this.changeUsernameValue}
+                  />
+                  <Button variant="contained" color="primary" className="Button">
+                    <Typography variant="subtitle1">Login</Typography>
+                  </Button>
+                  {
+                    (tabValue === 0 || tabValue === 1)
+                      && (
+                      <Box component="span">
+                        <br />
+                        <br />
+                        <Link to="/forgot-password" className="Button">Forgot Password?</Link>
+                        &nbsp; | &nbsp;
+                        <Link to="/register" className="Button">Register</Link>
+                      </Box>
+                      )
+                  }
+                </TabContainer>
+              </Paper>
+              {
+                (tabValue === 0 || tabValue === 1)
+                  && (
+                    <Typography variant="subtitle1">
+                      <br />
+                      Promosi? Viralin ajaaa..
+                    </Typography>
+                  )
+              }
+            </Container>
+          </Grid>
+          <Grid item xs={3} />
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default LoginScreen;
-;
