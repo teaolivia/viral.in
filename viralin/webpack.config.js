0b579acbefd5657bd144925f57c9f056ede6b2d5
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 01:33:53 +0700

    change how webpack load css

--- viralin/webpack.config.js
+++ viralin/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         loader: 'css-loader',
         query: {
           modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
+          localIdentName: '[name]'
         }
       }
     ]
