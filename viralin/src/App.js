8a1e0c66e693f61a0cacd3388b0de1448fd5354f
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 06:07:39 +0700

    refactor App Component to fullfill eslint

--- viralin/src/App.js
+++ viralin/src/App.js
@@ -1,22 +1,21 @@
-
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import LoginScreen from "Components/LoginScreen/LoginScreen"
-import RegisterScreen from "Components/RegisterScreen/RegisterScreen"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginScreen from 'Components/LoginScreen/LoginScreen';
+import RegisterScreen from 'Components/RegisterScreen/RegisterScreen';
 
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-    }
+    this.state = {};
   }
-  componentWillMount(){
-  }
+
   render() {
     return (
-      <Router>        
+      <Router>
         <div className="App">
           <Route path="/" exact component={LoginScreen} />
           <Route path="/login" exact component={LoginScreen} />
