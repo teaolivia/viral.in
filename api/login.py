a544bfba7e491b9fa4cda9897d7a00157edbe728
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-06-02 00:35:48 +0700

    login page logic finished

---
+++ api/login.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, render_template, request, session, abort, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return(redirect(url_for('home')))
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
