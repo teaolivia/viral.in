a337a741c7159a32a1676b2bc635fd40db57f8ac
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 03:08:36 +0700

    add grid system and typography

--- viralin/src/Components/LoginScreen/LoginScreen.js
+++ viralin/src/Components/LoginScreen/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 import './LoginScreen.css';
 
@@ -27,29 +26,48 @@ class LoginScreen extends React.Component {
     render () {
         return (
             <div className="LoginScreen">
-                <div className="LoginFormContainer">
-                    <Paper className = "Paper">
-                        <Tabs value={this.state.value} onChange={this.handleChange}>
-                            <Tab label="Akun Bisnis" />
-                            <Tab label="Akun Promotor" />
-                        </Tabs>
-                        {this.state.value === 0 && <TabContainer>
-                            s
-                            {/* <TextField
-                                id="filled-name"
-                                label="Name"
-                                className={classes.textField}
-                                value={values.name}
-                                onChange={this.setState({username : value})}
-                                margin="normal"
-                                variant="filled"
-                            /> */}
-                        </TabContainer>}
-                        {this.state.value === 1 && <TabContainer>
-                            Itesm Two
-                        </TabContainer>}
-                    </Paper>            
-                </div>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={0}
+                >    
+                    <Grid item xs={12}>
+                        <Typography className="Header" variant="h1" component="h2">
+                            VIRALIN
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}><br /></Grid>
+                    <Grid item xs={3}></Grid>
+                    <Grid item xs={6}>
+                        <Container>
+                            <Paper className = "Paper">
+                                <Tabs value={this.state.value} onChange={this.handleChange}>
+                                    <Tab label="Akun Bisnis" />
+                                    <Tab label="Akun Promotor" />
+                                </Tabs>
+                                {this.state.value === 0 && <TabContainer>
+                                    s
+                                    {/* <TextField
+                                        id="filled-name"
+                                        label="Name"
+                                        className={classes.textField}
+                                        value={values.name}
+                                        onChange={this.setState({username : value})}
+                                        margin="normal"
+                                        variant="filled"
+                                    /> */}
+                                </TabContainer>}
+                                {this.state.value === 1 && <TabContainer>
+                                    Itesm Two
+                                </TabContainer>}
+                            </Paper>            
+                        </Container>
+                    </Grid>
+                    <Grid item xs={3}></Grid>
+                    
+                </Grid>
             </div>
         );        
     }
