5c7ac9fc6a8221a0720272d57d704abcec00657e
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-02 06:16:24 +0700

    refactor TabContainer component to fullfill eslint

--- viralin/src/Components/TabContainer/TabContainer.js
+++ viralin/src/Components/TabContainer/TabContainer.js
@@ -1,16 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
 function TabContainer(props) {
-    return (
-      <Typography component="div" style={{ padding: 8 * 3 }}>
-        {props.children}
-      </Typography>
-    );
+  const { children } = props;
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
 }
-  
 TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
-};  
+  children: PropTypes.node.isRequired,
+};
 export default TabContainer;
