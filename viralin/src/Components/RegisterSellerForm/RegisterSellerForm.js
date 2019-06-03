15532bddfc6bff078a1a89a20f22c8eca42e3115
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-03 12:58:07 +0700

    RegisterForm Component for Seller

---
+++ viralin/src/Components/RegisterSellerForm/RegisterSellerForm.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class RegisterSellerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Typography variant="subtitle1">Registrasi Akun Bisnis</Typography>
+      </div>
+    );
+  }
+}
+
+export default RegisterSellerForm;
