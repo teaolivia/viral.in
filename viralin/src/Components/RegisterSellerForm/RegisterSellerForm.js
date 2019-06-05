84f8ab4dd31d16d728661459369dd9fc059d8264
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-05 15:12:45 +0700

    Add fetch provinsi, select form and its handler

--- viralin/src/Components/RegisterSellerForm/RegisterSellerForm.js
+++ viralin/src/Components/RegisterSellerForm/RegisterSellerForm.js
@@ -5,6 +5,12 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import FilledInput from '@material-ui/core/FilledInput';
+
+import fetchProvinsiApi from 'Api/fetchProvinsiApi';
 
 class RegisterSellerForm extends React.Component {
   constructor(props) {
@@ -16,27 +22,48 @@ class RegisterSellerForm extends React.Component {
       submittedJenisUsaha: '',
       namaPebisnis: '',
       submittedNamaPebisnis: '',
+      isProvinsiLoaded: false,
+      provinsiArray: [],
+      provinsi: '',
+      submittedProvinsi: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.changeNamaUsahaValue = this.changeNamaUsahaValue.bind(this);
     this.changeJenisUsahaValue = this.changeJenisUsahaValue.bind(this);
     this.changeNamaPebisnisValue = this.changeNamaPebisnisValue.bind(this);
+    this.changeProvinsiValue = this.changeProvinsiValue.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchProvinsi();
   }
 
   componentDidUpdate() {
-    const { submittedNamaUsaha, submittedJenisUsaha, submittedNamaPebisnis } = this.state;
+    const {
+      submittedNamaUsaha,
+      submittedJenisUsaha,
+      submittedNamaPebisnis,
+      submittedProvinsi,
+    } = this.state;
     console.log(`Nama Usaha: ${submittedNamaUsaha}`);
     console.log(`Jenis Usaha: ${submittedJenisUsaha}`);
     console.log(`Nama Usaha: ${submittedNamaPebisnis}`);
+    console.log(`Provsinsi Alamat: ${submittedProvinsi}`);
   }
 
   handleSubmit(event) {
-    const { namaUsaha, jenisUsaha, namaPebisnis } = this.state;
+    const {
+      namaUsaha,
+      jenisUsaha,
+      namaPebisnis,
+      provinsi,
+    } = this.state;
     event.preventDefault();
     this.setState({
       submittedNamaUsaha: namaUsaha,
       submittedJenisUsaha: jenisUsaha,
       submittedNamaPebisnis: namaPebisnis,
+      submittedProvinsi: provinsi,
     });
     console.log('form submitted');
   }
@@ -53,8 +80,37 @@ class RegisterSellerForm extends React.Component {
     this.setState({ namaPebisnis: event.target.value });
   }
 
+  changeProvinsiValue(event) {
+    this.setState({ provinsi: event.target.value });
+  }
+
+  fetchProvinsi() {
+    const data = fetchProvinsiApi;
+    data.then(
+      (result) => {
+        this.setState({
+          isProvinsiLoaded: true,
+        });
+        if (!result.error) {
+          this.setState({ provinsiArray: result.semuaprovinsi });
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }
+
   render() {
-    const { namaUsaha, jenisUsaha, namaPebisnis } = this.state;
+    const {
+      namaUsaha,
+      jenisUsaha,
+      namaPebisnis,
+      isProvinsiLoaded,
+      provinsi,
+      provinsiArray,
+    } = this.state;
+    const provinsis = provinsiArray.map(i => <option key={i.id} value={i.id}>{i.nama}</option>);
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -89,6 +145,21 @@ class RegisterSellerForm extends React.Component {
             value={namaPebisnis}
             onChange={this.changeNamaPebisnisValue}
           />
+          { isProvinsiLoaded
+          && (
+            <FormControl className="FormControl" variant="filled" fullWidth>
+              <InputLabel htmlFor="filled-provinsi-native-simple">Provinsi</InputLabel>
+              <Select
+                native
+                value={provinsi}
+                onChange={this.changeProvinsiValue}
+                input={<FilledInput name="age" id="filled-age-native-simple" />}
+              >
+                <option value="" />
+                { provinsis }
+              </Select>
+            </FormControl>
+          )}
           <Button
             variant="contained"
             color="primary"
