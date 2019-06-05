1f4663808cc5afde1ea7549066bc90122316c84e
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-05 15:50:45 +0700

    Api for fetching kabupaten kota

--- viralin/src/Api/fetchKabupatenKotaApi.js
+++ viralin/src/Api/fetchKabupatenKotaApi.js
@@ -1,10 +1,9 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const targetUrl = 'https://dev.farizdotid.com/api/daerahindonesia/provinsi/';
-const fetchProvinsiApi = (id) => {
-  fetch(`${proxyUrl}${targetUrl}${id}/kabupaten`, {
+const fetchKabupatenKotaApi = id => fetch(`${proxyUrl}${targetUrl}${id}/kabupaten`,
+  {
     crossDomain: true,
     method: 'GET',
   })
-    .then(res => res.json());
-};
-export default fetchProvinsiApi;
+  .then(res => res.json());
+export default fetchKabupatenKotaApi;
