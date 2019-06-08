5af166228cda5ddd7a3cfc0f0dcb792b0ff91637
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-08 22:33:17 +0700

    add circular progress for indicating loading province and kabupaten/kota

--- viralin/src/Components/RegisterSellerForm/RegisterSellerForm.js
+++ viralin/src/Components/RegisterSellerForm/RegisterSellerForm.js
@@ -10,6 +10,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import FilledInput from '@material-ui/core/FilledInput';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import fetchProvinsiApi from 'Api/fetchProvinsiApi';
 import fetchKabupatenKotaApi from 'Api/fetchKabupatenKotaApi';
@@ -299,7 +300,12 @@ class RegisterSellerForm extends React.Component {
             onChange={this.handleInputChange}
           />
           <br />
-          { isLoadingProvinsi && <Typography>Mengambil data provinsi...</Typography> }
+          { isLoadingProvinsi && (
+            <div>
+              <CircularProgress />
+              <Typography>Mengambil data provinsi...</Typography>
+            </div>
+          ) }
           { (isProvinsiLoaded && !isLoadingProvinsi)
           && (
             <FormControl className="FormControl" variant="filled" fullWidth>
@@ -317,7 +323,12 @@ class RegisterSellerForm extends React.Component {
           )}
           <br />
           <br />
-          { isLoadingKabupatenKota && <Typography>Mengambil data kabupaten/kota...</Typography> }
+          { isLoadingKabupatenKota && (
+            <div>
+              <CircularProgress />
+              <Typography>Mengambil data kabupaten/kota...</Typography>
+            </div>
+          ) }
           { (isKabupatenKotaLoaded && !isLoadingKabupatenKota)
           && (
             <FormControl className="FormControl" variant="filled" fullWidth>
