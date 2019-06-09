e05dc0e5bc39b09c03e539e0328aba263b93bda5
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-09 23:42:29 +0700

    Add admin avatar

--- viralin/src/Components/AdminDashboardScreen/AdminDashboardScreen.js
+++ viralin/src/Components/AdminDashboardScreen/AdminDashboardScreen.js
@@ -1,25 +1,60 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Person from '@material-ui/icons/Person';
+import Typography from '@material-ui/core/Typography';
 import 'Components/AdminDashboardScreen/AdminDashboardScreen.css';
 
 class AdminDashboardScreen extends React.Component {
   constructor(props) {
     super(props);
     this.setState({
-      totalContent: 0,
-      totalActiveContent: 0,
-      totalPebisnis: 0,
-      totalActivePebisnis: 0,
-      totalPromotor: 0,
-      totalActivePromotor: 0,
+      buttons: [
+        {
+          imageUrl: '',
+          targetUrl: '',
+          name: 'Konten',
+          header: 'Kelola Konten Viral',
+          count: 0,
+          activeCount: 0,
+        },
+        {
+          imageUrl: '',
+          targetUrl: '',
+          name: 'Pebisnis',
+          header: 'Database Pebisnis',
+          count: 0,
+          activeCount: 0,
+        },
+        {
+          imageUrl: '',
+          targetUrl: '',
+          name: 'Promotor',
+          header: 'Database Promotor',
+          count: 0,
+          activeCount: 0,
+        },
+      ],
     });
   }
 
   render() {
-    return(
+    return (
       <div className="AdminDashboardScreen">
-        hello
+        <Grid
+          container
+          direction="row-reverse"
+          alignItems="center"
+          spacing={0}
+          className="Top"
+        >
+          <Avatar><Person /></Avatar>
+          <span>&nbsp;</span>
+          <span>&nbsp;</span>
+          <Typography>Admin</Typography>
+        </Grid>
       </div>
     );
   }
