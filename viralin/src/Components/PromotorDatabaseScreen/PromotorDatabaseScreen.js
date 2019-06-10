217bf968577be45b28684fced0805dc3456bd060
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-10 17:39:58 +0700

    Promotor Database Screen Component

---
+++ viralin/src/Components/PromotorDatabaseScreen/PromotorDatabaseScreen.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+class PromotorDatabaseScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state();
+  }
+
+  render() {
+    return (
+      <div>Promotor database</div>
+    );
+  }
+}
+
+export default PromotorDatabaseScreen;
