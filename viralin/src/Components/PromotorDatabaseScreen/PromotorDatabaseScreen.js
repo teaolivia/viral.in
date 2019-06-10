d91cad9279cfd1010f3bbc5b3923e52e68a88c64
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-10 17:57:30 +0700

    Add 'admin' avatar

--- viralin/src/Components/PromotorDatabaseScreen/PromotorDatabaseScreen.js
+++ viralin/src/Components/PromotorDatabaseScreen/PromotorDatabaseScreen.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import Person from '@material-ui/icons/Person';
 import 'Components/PromotorDatabaseScreen/PromotorDatabaseScreen.css';
 
 class PromotorDatabaseScreen extends React.Component {
@@ -11,7 +15,38 @@ class PromotorDatabaseScreen extends React.Component {
 
   render() {
     return (
-      <div className="PromotorDatabaseScreen">Promotor database</div>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        spacing={0}
+        className="Top"
+      >
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          spacing={0}
+          className="Top"
+          xs={6}
+        >
+          kiri
+        </Grid>
+        <Grid
+          container
+          direction="row-reverse"
+          alignItems="center"
+          spacing={0}
+          className="Top"
+          xs={6}
+        >
+          <Avatar><Person /></Avatar>
+          <span>&nbsp;</span>
+          <span>&nbsp;</span>
+          <Typography>Admin</Typography>
+        </Grid>
+
+      </Grid>
     );
   }
 }
