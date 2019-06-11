67c0df928a7c094eb88e94696e96c0274be649ac
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-12 05:40:50 +0700

    Boilerplate for AdminManageKontenViral component

---
+++ viralin/src/Components/AdminManageKontenViral/AdminManageKontenViral.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import 'Components/AdminManageKontenViral/AdminManageKontenViral.css';
+
+class AdminManageKontenViral extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="AdminManageKontenViral" />
+    );
+  }
+}
+
+export default AdminManageKontenViral;
